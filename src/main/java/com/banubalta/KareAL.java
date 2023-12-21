b6bcd6416d19000fb67a8f160298966ad62c9fb1
Banu balta <nbanu.balta@gmail.com>
2023-12-21 17:31:26 +0300

    Initial commit

---
+++ src/main/java/com/banubalta/KareAL.java
@@ -0,0 +1,18 @@
+package com.banubalta;
+
+public class KareAL {
+
+    public static void main(String[] args) {
+        System.out.println("Karesi: " + kareAl(5, 2));
+        System.out.println("TOPLAMA:" + topla(5, 2, 4));
+    }
+
+    public static float topla(int sayi1, int sayi2, int sayi3) {
+        return sayi1+ sayi2 + sayi3;
+    }
+
+    private static double kareAl(double taban, double üs) {
+        return Math.pow(taban, üs);
+    }
+}
+
---
+++ src/main/java/com/banubalta/Metodlar.java
@@ -0,0 +1,21 @@
+package com.banubalta;
+
+public class Metodlar {
+
+
+    public static void cizgiCiz(){
+        System.out.println("- - -");
+    }
+
+    public static void main(String[] args) {
+
+        //   System.out.println("-------------------------");
+        //   System.out.println("-------------------------");
+
+
+        for (int i = 0; i < 5; i++) {
+            cizgiCiz();
+        }
+
+    }
+}
